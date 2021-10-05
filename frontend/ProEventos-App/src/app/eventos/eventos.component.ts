62643d9c691bff5ec2dc73455942400d3ca2e8bb
Heleomar <mazinhoemail@gmail.com>
2021-10-05 09:59:40 -0300

    Angular - layout e Pesquisa por filtro

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 
@@ -8,6 +9,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EventosComponent implements OnInit {
     public eventos: any;
+    public eventosFIltrados: any = [];
+    public mostrarImagem: boolean = true;
+    private _filtroLista: string = "";
+
+    public get filtroLista(): string{
+        return this._filtroLista;
+    }
+
+    public set filtroLista(value: string){
+        this._filtroLista = value;
+        this.eventosFIltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+    }
+
+    filtrarEventos(filtrarPor: string): any{
+        filtrarPor = filtrarPor.toLocaleLowerCase();
+        return this.eventos.filter(
+            (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+        );
+    }
 
     constructor(private http: HttpClient) {}
 
@@ -17,8 +37,15 @@ export class EventosComponent implements OnInit {
 
     public getEventos(): void {
         this.http.get('https://localhost:5001/api/evento').subscribe(
-            response => this.eventos = response,
+            response => {
+                this.eventos = response;
+                this.eventosFIltrados = this.eventos
+            },
             error => console.log(error)
         );
+    }
+
+    alterarImagem(){
+        this.mostrarImagem = !this.mostrarImagem
     }
 }
